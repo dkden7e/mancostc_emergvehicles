f633b61b1b5baf5f333830882f914524006f0669
Benito "DK_Den7e" L <benitoll1993@gmail.com>
2022-05-17 22:45:35 -0700

    Initial commit

---
+++ [tc vehiculos emergencias]/tc_scs/fxmanifest.lua
@@ -0,0 +1,30 @@
+-- Generated with AltTool
+
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+ 'handling.meta',
+ 'vehicles.meta',
+ 'carcols.meta',
+ 'carvariations.meta',
+ 'vehiclelayouts.meta',
+ '*contentunlocks*.meta',
+ 'dlctext.meta',
+ 'explosion.meta',
+ 'explosionfx.dat',
+ 'weaponarchetypes.meta',
+ 'audio/config/*.*'
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' '*contentunlocks*.meta'
+data_file 'DLCTEXT_FILE' 'dlctext.meta'
+data_file 'EXPLOSION_INFO_FILE' 'explosion.meta'
+data_file 'EXPLOSIONFX_FILE' 'explosionfx.dat'
+data_file 'WEAPON_METADATA_FILE' 'weaponarchetypes.meta'
+data_file 'AUDIO_GAMEDATA' 'audio/config/emsroamer_game.dat'
